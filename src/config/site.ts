3b5ef23f8e6b13bc2f68fb1de13701e47f6d952d
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2024-03-12 20:43:58 +0000

    update nav links

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,12 +4,8 @@ export const siteConfig = {
   name: "Where in the world?",
   description: "Site that shows countries data",
   // We can just keep adding objects to this which are looped over in main-nav.ts - nice!
-  mainNav: [
-    {
-    },
-  ],
+  mainNav: [{}],
   links: {
-    github: "https://github.com/HK273",
-    docs: "https://ui.shadcn.com",
+    github: "https://github.com/HK273/countries-api",
   },
 };
